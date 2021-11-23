649454cc957098acd8371bb77fe2be34e3cde74a
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-11-23 15:12:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (rideDistance > 2500) {
+    return 'No can do.';
+  }
+  else if (rideDistance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+const cityMessage = cityName === 'NYC' ? "Ok, sounds good." : "No go.";
+return cityMessage;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){  
   // Write your code here!
+switch (tipAmount) {
+  case 'generous':
+    return "Thank you so much.";
+    break;
+  case 'not as generous':
+    return "Thank you.";
+    break;
+  default:
+    return "Bye."
+    break;
+}
 }
